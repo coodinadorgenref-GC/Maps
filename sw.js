7cbc368b789896f90871d2b256688e423b567c49
coodinadorgenref-GC <coodinadorgenref@gmail.com>
2026-09-26 06:49:36 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // para que la herramienta funcione sin señal una vez usada al menos una vez
 // en la zona donde se necesita.
 
-const CACHE_SHELL = 'rutas-gc-shell-v38';
+const CACHE_SHELL = 'rutas-gc-shell-v39';
 const CACHE_TILES = 'rutas-gc-tiles-v1';
 
 const SHELL_ASSETS = [
